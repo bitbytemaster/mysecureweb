91e5a4597260bc04ec797d9d02c045736eae471d
tigerich <cxd3000@gmail.com>
2014-04-22 22:10:33 +0800

    added login, main etc module and license text

---
+++ code.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import web
+from web import form
+
+web.config.debug = True
+#web.config.debug = False
+
+urls = (
+    '', 'main',
+    '/', 'main',
+    '/login', 'login',
+    '/logout', 'logout'
+)
+
+app = web.application(urls, globals())
+application = app.wsgifunc()
+curr_dir = os.path.dirname(__file__)
+web.config.session_parameters['cookie_path'] = '/'
+
+ds = web.session.DiskStore(curr_dir + '/sessions')
+#db = web.database(dbn='mysql', db='mysecureweb', user='dev', pw='Intel@123')
+#ds = web.session.DBStore(db, 'sessions')
+if web.config.get('_session') is None:
+    session = web.session.Session( app, ds, initializer={'username':'anonymous', 'loggedin':False} )
+    web.config._session = session
+else:
+    session = web.config._session
+
+render = web.template.render(curr_dir + '/templates/', base='layout', globals={'context': session})
+
+allowed = (
+    ('chenric','999'),
+)
+
+class main:
+    def GET(self):
+        if session.get( 'loggedin', True ):
+            return render.main()
+        raise web.seeother('/login')
+            
+class login:
+    def GET(self):
+        if session.get( 'loggedin', True ):
+            raise web.seeother('/')
+        else:
+            return render.login()
+
+    def POST(self):
+        wi = web.input()
+        if (wi.username, wi.password) in allowed:
+            session.loggedin = True
+            session.username = wi.username
+            raise web.seeother('/')
+        else:
+            
+            return render.login()
+
+class logout:
+    def GET(self):
+        session.kill()
+        ds.cleanup(0.002)  
+        raise web.seeother('/login')
+
+#END OF FILE
---
+++ static/js/login.js
@@ -0,0 +1,35 @@
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+var delay=50;
+var currentChar=1;
+
+function type(dest_id, text)
+{
+    var dest=document.getElementById(dest_id);
+    if (dest)
+    {
+      dest.innerHTML=text.substr(0, currentChar);
+      currentChar++
+      if (currentChar>text.length)
+      {
+        currentChar=1;
+        setTimeout("type()", 5000);
+      }
+      else
+      {
+        setTimeout("type()", delay);
+      }
+    }
+}
+
+function startTyping(textParam, delayParam, destinationParam)
+{
+  text=textParam;
+  delay=delayParam;
+  currentChar=1;
+  destination=destinationParam;
+  type();
+}
+
+//-->
+//</SCRIPT>
